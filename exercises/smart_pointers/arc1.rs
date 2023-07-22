c4b3c8f6700ae82e6e4d9ef08615297339611b6f
cuppar <cuppar@163.com>
2023-07-22 17:36:00 +0800

    s

--- exercises/smart_pointers/arc1.rs
+++ exercises/smart_pointers/arc1.rs
@@ -21,25 +21,29 @@
 //
 // Execute `rustlings hint arc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers); // TODO
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
-        let child_numbers = // TODO
+        let child_numbers = Arc::clone(&shared_numbers); // TODO
         joinhandles.push(thread::spawn(move || {
             let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
+            thread::sleep(Duration::from_secs(3));
             println!("Sum of offset {} is {}", offset, sum);
+            sum
         }));
     }
+    let mut sum = 0;
     for handle in joinhandles.into_iter() {
-        handle.join().unwrap();
+        sum += handle.join().unwrap();
     }
+    println!("total: {}", sum);
 }
