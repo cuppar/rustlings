865982c117c8e1d8ef6f906c518d46fd70799bae
cuppar <cuppar@163.com>
2023-07-22 21:24:13 +0800

    finish

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            Default::default()
+        } else {
+            let splited: Vec<&str> = s.split(',').collect();
+            if splited.len() != 2 {
+                return Person::default();
+            }
+            let name = splited[0].to_owned();
+            if let Ok(age) = splited[1].parse::<usize>() {
+                if name.is_empty() {
+                    Person::default()
+                } else {
+                    Person { name, age }
+                }
+            } else {
+                Person::default()
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,25 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let splited = s.split(',').collect::<Vec<&str>>();
+        if splited.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        let name = splited[0].to_string();
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let age = splited[1].parse::<usize>();
+        if let Err(e) = age {
+            return Err(ParsePersonError::ParseInt(e));
+        }
+        Ok(Person {
+            name,
+            age: age.unwrap(),
+        })
     }
 }
 
